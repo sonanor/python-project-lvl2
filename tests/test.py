0ff4d374316a8588c1718aadba9d83f81377633a
Sona Esayan <sonaevn@gmail.com>
2021-12-03 18:54:48 +0300

    Change files structure

--- tests/test.py
+++ tests/test.py
@@ -2,5 +2,5 @@ from gendiff.diff_generator import generate_diff
 
 
 def test_simple_case():
-    expected = open('./fixtures/simple_result.txt').read()
+    expected = open('fixtures/simple_result.txt').read()
     assert generate_diff('./fixtures/file1.json', './fixtures/file2.json') == expected
