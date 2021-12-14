bcf29551a5f54d048d50aff6003f881ddd19ff47
Sona Esayan <sonaevn@gmail.com>
2021-12-15 02:52:04 +0300

    Fixed linter requirements

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -3,7 +3,8 @@ from .diff_builder import create_diff
 from .formatters.format_output import create_output_result
 
 
-def generate_diff(first_path: str, second_path: str, output_format='stylish') -> str:
+def generate_diff(first_path: str, second_path: str,
+                  output_format='stylish') -> str:
     first_file_dict = parse_file(first_path)
     second_file_dict = parse_file(second_path)
     diff = create_diff(first_file_dict, second_file_dict)
